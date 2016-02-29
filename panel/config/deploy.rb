4f95e1b636433c98034f437716c34783fdcb806f
Daisuke TASAKI <tasaki@i3-systems.com>
2016-02-29 22:35:26 +0900

    capify

---
+++ panel/config/deploy.rb
@@ -0,0 +1,64 @@
+# config valid only for current version of Capistrano
+lock '3.4.0'
+
+set :application, 'panel'
+set :repo_url, 'git@github.com:capistrano/capistrano.git'
+
+# Default branch is :master
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/app/nslides/panel'
+
+# Default value for :scm is :git
+set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+
+# Default value for linked_dirs is []
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'tmp/sessions', 'vendor/bundle', 'public/system')
+
+# Default value for default_env is {}
+set :default_env, { path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH" }
+
+# Default value for keep_releases is 5
+set :keep_releases, 5
+
+# capistrano/rbenv
+set :rbenv_type, :system
+set :rbenv_ruby, '2.3.0'
+
+# capistrano/bundler
+set :bundle_path, -> { shared_path.join('vendor', 'bundle') }
+set :bundle_binstubs, nil
+#set :bundle_without, %w{development test deployment}.join(' ')
+set :bundle_without, %w{test deployment}.join(' ')
+set :bundle_flags, '--deployment --quiet'
+
+# capistrano-copy-subdir
+set :deploy_via, :copy_subdir
+set :deploy_subdir, 'panel'
+
+
+namespace :deploy do
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+end
